25c881f7f88779401f3bd680281106aab996d67c
Someonelse2309 <117884155+Someonelse2309@users.noreply.github.com>
2023-02-15 18:57:31 +0700

    Add files via upload

---
+++ 4_D_71220845.py
@@ -0,0 +1,31 @@
+ulg = 1
+smuanama = ['']
+smuamember = ['']
+a = 1
+b = 1
+while ulg == 1:
+    print ("======= Selamat Datang =======")
+    print ("Masukan Pilihan Anda\n 1. Menambah Data\n 2. Melihat Data\n 3. Keluar")
+    pil1 = int(input(">> "))
+    if pil1 == 1:
+        nama = str(input("\nMasukan Nama Pengguna\n>> "))
+        member = str(input("Masukan Pilihan Member\n>> "))
+        smuanama.append(nama)
+        smuamember.append(member)
+        ulg = 1
+        print ("\n")
+    elif pil1 == 2:
+        print ("Nama - Membership")
+        jmlh = len(smuanama)
+        while a < jmlh:
+            print (f"{a}. {smuanama[b]} - {smuamember[b]}")
+            a += 1
+            b += 1
+        ulg = 1
+        print ("\n")
+    elif pil1 == 3:
+        print ("Terima Kasih")
+        ulg = 0
+    else:
+        print ("Maaf Input Anda Invalid\n")
+        ulg = 1
