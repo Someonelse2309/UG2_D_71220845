953d7a274406a96e6786810db8c6528546bca3b4
Someonelse2309 <117884155+Someonelse2309@users.noreply.github.com>
2023-02-15 19:15:55 +0700

    Add files via upload

--- 3_D_71220845.py
+++ 3_D_71220845.py
@@ -5,21 +5,21 @@ ttlhrg = 0
 brg = input("Masukan Barang Anda (Pisahkan Dengan Koma)\n>> ").split(",")
 count = len(brg)
 while count > a:
-    hrg = int(input(f"Masukan Harga Barang {brg[a]}\n>> "))
-    if hrg > 25000000:
+    hrg = int(input(f"\nMasukan Harga Barang {brg[a]}\n>> "))
+    if hrg >= 25000000:
         print (f"Diskon 25%")
         dskn = hrg * 0.25
         hrgakh = hrg - dskn
-        print (f"Dapat Diskon Rp. {dskn}\nHarga setelah diskon {hrgakh}\nHarga sebelum diskon {hrg}")
+        print (f"\nHarga Sebelum Diskon \t: Rp. {int(hrg)}\nDapat Diskon (25%) \t: Rp. {int(dskn)}\nHarga Setelah Diskon \t: Rp. {int(hrgakh)}")
         ttlhrg += hrgakh
         ttldskn += dskn
         a += 1
         b += 1
-    elif hrg > 10000000:
+    elif hrg >= 10000000:
         print (f"Diskon 10%")
         dskn = hrg * 0.1
         hrgakh = hrg - dskn
-        print (f"Dapat Diskon Rp. {dskn}\nHarga setelah diskon {hrgakh}\nHarga sebelum diskon {hrg}")
+        print (f"\nHarga Sebelum Diskon \t: Rp. {int(hrg)}\nDapat Diskon (10%) \t: Rp. {int(dskn)}\nHarga Setelah Diskon \t: Rp. {int(hrgakh)}")
         ttlhrg += hrgakh
         ttldskn += dskn
         a += 1
@@ -27,9 +27,9 @@ while count > a:
     else:
         hrgakh = hrg
         dskn = 0
-        print (f"Dapat Diskon Rp. {dskn}\nHarga setelah diskon {hrgakh}\nHarga sebelum diskon {hrg}")
+        print (f"\nHarga Sebelum Diskon \t: Rp. {int(hrg)}\nDapat Diskon (0%) \t: Rp. {int(dskn)}\nHarga Setelah Diskon \t: Rp. {int(hrgakh)}")
         ttlhrg += hrgakh
         a += 1
         b += 1
-print (f"Harga Total {ttlhrg}\nTotal Diskon {ttldskn}")
+print (f"\nHarga Total\t\t: Rp. {ttlhrg}\nTotal Diskon\t\t: Rp. {ttldskn}\n")
 
